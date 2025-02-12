36975aca0a3d99bd54d800bda1a0a31b7c43a607
Eliter Daneo <eliterdaneo13@gmail.com>
2025-02-12 09:02:10 +0700

    tambahan fitur error

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -58,30 +58,71 @@
                             @csrf
                             <div class="mb-3">
                                 <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                <input type="email" name="email" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp" value="{{ old('email') }}">
+                                <input type="email" name="email"
+                                    class="form-control @error('email')
+                                    is-invalid
+                                @enderror"
+                                    id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Password</label>
-                                <input type="password" name="password" class="form-control" id="exampleInputPassword1"
-                                    value="{{ old('password') }}">
+                                <input type="password" name="password"
+                                    class="form-control @error('password')
+                                    is-invalid
+                                @enderror"
+                                    id="exampleInputPassword1" value="{{ old('password') }}">
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Nama</label>
-                                <input type="text" name="name" class="form-control" id="exampleInputPassword1"
-                                    value="{{ old('name') }}">
+                                <input type="text" name="name"
+                                    class="form-control @error('name')
+                                    is-invalid
+                                @enderror"
+                                    id="exampleInputPassword1" value="{{ old('name') }}">
+                                @error('password')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Role</label>
-                                <select class="form-select" name="role" aria-label="Default select example">
+                                <select
+                                    class="form-control @error('role')
+                                    is-invalid
+                                @enderror"
+                                    name="role" aria-label="Default select example">
                                     <option selected>Open this select menu</option>
                                     <option value="admin">Admin</option>
                                     <option value="kasir">Kasir</option>
                                 </select>
+                                @error('role')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <div class="mb-3">
                                 <label for="exampleInputPassword1" class="form-label">Image</label>
-                                <input type="file" name="image" class="form-control">
+                                <input type="file" name="image"
+                                    class="form-control @error('image')
+                                    is-invalid
+                                @enderror">
+                                @error('image')
+                                    <div class="invalid-feedback">
+                                        {{ $message }}
+                                    </div>
+                                @enderror
                             </div>
                             <button type="submit" class="btn btn-primary">Submit</button>
                         </form>
